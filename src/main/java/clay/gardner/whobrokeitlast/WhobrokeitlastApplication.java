90a834efeac503dad705a779fcf83f3ce85e51a4
gardncl <gardncl@gmail.com>
2022-03-15 20:00:52 -0600

    first commit

---
+++ src/main/java/clay/gardner/whobrokeitlast/WhobrokeitlastApplication.java
@@ -0,0 +1,13 @@
+package clay.gardner.whobrokeitlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhobrokeitlastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhobrokeitlastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clay/gardner/whobrokeitlast/WhobrokeitlastApplicationTests.java
@@ -0,0 +1,13 @@
+package clay.gardner.whobrokeitlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhobrokeitlastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
