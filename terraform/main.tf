8724e65d389d3ecaab7677dc883e31910f821da1
gardncl <gardncl@gmail.com>
2022-03-17 20:01:02 -0600

    fix tf warning

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
+  required_providers {
+    aws = {
+      version = "~> 2.36.0"
+    }
+  }
   required_version = ">=0.12.13"
   backend "s3" {
     bucket         = "wbil-tf-bucket"
@@ -8,12 +13,6 @@ terraform {
     dynamodb_table = "aws-locks"
     encrypt        = true
   }
-}
-
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
-provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.36.0"
 }
 
 # Call the seed_module to build our ADO seed info
